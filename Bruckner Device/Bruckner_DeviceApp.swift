df1b2523781679b16109a423a8d6cc461c4a06f7
Spencer Jones <sjonesdrums@gmail.com>
2025-02-22 19:37:02 -0600

    Initial Commit

---
+++ Bruckner Device/Bruckner_DeviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bruckner_DeviceApp.swift
+//  Bruckner Device
+//
+//  Created by Spencer Jones on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Bruckner_DeviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
